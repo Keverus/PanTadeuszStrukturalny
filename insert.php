1231b01ea95ddb5be5d2ecf461435350893328b6
Fiona84 <kaska.wojcik@gmail.com>
2015-11-11 22:30:07 +0100

    mailing z refleksja w mysqlu

--- insert.php
+++ insert.php
@@ -17,4 +17,32 @@
   		exit;
 	}
 
+	$url = 'https://mandrillapp.com/api/1.0/messages/send.json';
+	$params = [
+		'message' => array(
+			'subject' => $title,
+			'text' => $reflection,
+			'html' => '<p>'.$content.'</p>',
+			'from_email' => 'wojcikk@v-ie.uek.krakow.pl',
+			'to' => array(
+				array(
+					'email' => 'wojcikk@wizard.uek.krakow.pl',
+					'name' => 'Katarzyna Wójcik'
+				)
+			)
+		)
+	];
+
+	$params['key'] = '1G6Ee5e6hCDaJTiU4QVkgw';
+	$params = json_encode($params);
+	$ch = curl_init(); 
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+
+	$head = curl_exec($ch); 
+	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+	curl_close($ch);
+
 	header("Location: ".$_SERVER['HTTP_REFERER']);
